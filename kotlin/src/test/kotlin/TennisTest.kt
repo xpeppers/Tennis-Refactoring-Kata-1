377ed716ea61722fc0b13621114f9dc229dff20e
Alessandro Piccioli <ap@zextras.com>
2020-06-24 11:40:47 +0200

    mob next [ci-skip]

--- kotlin/src/test/kotlin/TennisTest.kt
+++ kotlin/src/test/kotlin/TennisTest.kt
@@ -1,3 +1,4 @@
+import org.junit.Assert
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
@@ -5,7 +6,17 @@ import org.junit.Test
 class TennisTest {
     
     @Test
-    fun `test sample`() {
-        assertTrue(true)
+    fun `one point player1`() {
+        val game = TennisGame1("player1", "player2")
+        game.wonPoint("player1")
+        Assert.assertEquals("Fifteen-Love", game.getScore())
+    }
+
+    @Test
+    fun `same point`() {
+        val game = TennisGame1("player1", "player2")
+        game.wonPoint("player1")
+        game.wonPoint("player2")
+        Assert.assertEquals("Fifteen-All", game.getScore())
     }
 }
