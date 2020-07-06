fcdf8795f2330b58d93f4c616ac63afc167766f5
Pietro Di Bello <pierodibello@gmail.com>
2020-07-06 23:50:45 +0200

    move to next game state then a player makes a point

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -2,17 +2,19 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
 
     private var player1Points: Int = 0
     private var player2Points: Int = 0
-    private val gameState = Draw()
+    private var gameState: GameState = Draw()
 
     override fun wonPoint(playerName: String) {
         if (playerName === "player1")
             player1Points += 1
         else
             player2Points += 1
+
+        gameState = nextGameState()
     }
 
     override fun getScore(): String {
-        return nextGameState().score()
+        return gameState.score()
     }
 
     private fun nextGameState(): GameState {
