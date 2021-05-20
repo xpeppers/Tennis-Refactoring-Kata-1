fe9607a9b22dc66f22a6191dc88c3dce8a13e27c
ironzani <ivan.ronzani@aptos.com>
2021-05-20 10:07:10 +0200

    Test basico, punteggio di parità

--- csharp/Tennis/TennisTests.cs
+++ csharp/Tennis/TennisTests.cs
@@ -1,17 +1,41 @@
-using System;
 using NUnit.Framework;
 
 namespace Tennis
 {
-    [TestFixture]
-    public class TennisTests
-    {
-        [Test]
-        public void TestExample()
-        {
-            Assert.True(false);
-        }
+	[TestFixture]
+	public class TennisTests
+	{
+		private readonly string player1 = "player1";
+		private readonly string player2 = "player2";
 
-    }
+		[Test]
+		public void TestBasic()
+		{
+			TennisGame1 tennisGame1 = new TennisGame1(this.player1, this.player2);
+			tennisGame1.WonPoint(this.player1);
+			Assert.AreEqual("Fifteen-Love", tennisGame1.GetScore());
+		}
+
+		[Test]
+		public void TestDeuce()
+		{
+			TennisGame1 tennisGame1 = new TennisGame1(this.player1, this.player2);
+			Assert.AreEqual("Love-All", tennisGame1.GetScore());
+
+			tennisGame1.WonPoint(this.player1);
+			tennisGame1.WonPoint(this.player2);
+			Assert.AreEqual("Fifteen-All", tennisGame1.GetScore());
+
+			tennisGame1.WonPoint(this.player1);
+			tennisGame1.WonPoint(this.player2);
+			Assert.AreEqual("Thirty-All", tennisGame1.GetScore());
+
+			tennisGame1.WonPoint(this.player1);
+			tennisGame1.WonPoint(this.player2);
+			Assert.AreEqual("Deuce", tennisGame1.GetScore());
+
+		}
+
+	}
 }
 
